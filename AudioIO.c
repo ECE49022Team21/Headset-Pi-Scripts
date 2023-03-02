0770f25e58ee64a3db2b88b23c85100065f8bab1
Ishaan-Laroia <77766970+Ishaan-Laroia@users.noreply.github.com>
2023-03-02 13:49:48 -0500

    output status reg

--- AudioIO.c
+++ AudioIO.c
@@ -276,6 +276,8 @@ void ReadCompass()
     outZ = outZ | data[0];
 
     printf("Compass Data: (%d, %d, %d)\n", outX, outY, outZ);
+    Read(Compass_Address, Reg_Compass_Status, data, 1, true);
+    printf("Status: %d\n", data[0]);
 }
 
 int main()
